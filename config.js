5e3c56cb688de51e17c6522d9b5edb44b19ce8e0
gaedsonjunior-sudo <ga.edson.junior@gmail.com>
2026-02-12 11:33:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Configuração do Supabase
 // IMPORTANTE: Substitua estas variáveis pelas suas credenciais do Supabase
 
-const SUPABASE_URL = 'SUA_URL_DO_SUPABASE';
-const SUPABASE_ANON_KEY = 'SUA_CHAVE_ANON_DO_SUPABASE';
+const SUPABASE_URL = 'https://goxfwzqftyqagpblzowd.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdveGZ3enFmdHlxYWdwYmx6b3dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MzE4MTQsImV4cCI6MjA4NjMwNzgxNH0.-X1E2IqizXDtRIs7N0sAery9Ni1TS19DeWEEfBSXgNc';
 
 // Inicialização do cliente Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
